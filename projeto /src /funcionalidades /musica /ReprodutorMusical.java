a00e52976ba54f3d2be17597ce3325c6dc6e7eed
Lucas <lucasmattioli013@gmail.com>
2024-08-16 15:51:21 -0300

    Update ReprodutorMusical.java

--- projeto /src /funcionalidades /musica /ReprodutorMusical.java
+++ projeto /src /funcionalidades /musica /ReprodutorMusical.java
@@ -1,1 +1,10 @@
+package funcionalidades.musica;
 
+public interface ReprodutorMusical {
+    void tocar();
+    void pausar();
+    void selecionarMusica();
+    void aumentarVolume();
+    void abaixarVolume();
+
+}
