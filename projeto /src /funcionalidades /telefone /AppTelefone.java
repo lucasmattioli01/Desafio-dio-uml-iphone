d2edc9a34e2174d6f990dc34bdc548c465230aed
Lucas <lucasmattioli013@gmail.com>
2024-08-16 15:53:51 -0300

    Update AppTelefone.java

--- projeto /src /funcionalidades /telefone /AppTelefone.java
+++ projeto /src /funcionalidades /telefone /AppTelefone.java
@@ -1,1 +1,19 @@
+package funcionalidades.telefone;
 
+public class AppTelefone {
+    public void ligar() {
+        System.out.println("Ligando...");
+    }
+
+    public void atender() {
+        System.out.println("Atendendo...");
+    }
+
+    public void desligar() {
+        System.out.println("Desligando...");
+    }
+
+    public void inicarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+}
