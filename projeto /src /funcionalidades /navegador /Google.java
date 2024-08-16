f724a401ca2294e4661dd6c29d60e98959145df6
Lucas <lucasmattioli013@gmail.com>
2024-08-16 15:53:18 -0300

    Update Google.java

--- projeto /src /funcionalidades /navegador /Google.java
+++ projeto /src /funcionalidades /navegador /Google.java
@@ -1,1 +1,20 @@
+package funcionalidades.navegador;
+
+public class Google implements Navegador {
+    public void pesquisar() {
+        System.out.println("Pesquisando no Google...");
+    }
+
+    public void exibirPagina() {
+        System.out.println("Exibindo página no Google...");
+    }
+
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba no Google...");
+    }
+
+    public void atualizarPagina() {
+        System.out.println("Atualizando página no Google...");
+    }
+}
 
