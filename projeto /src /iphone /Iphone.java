2d204c323fd526df9cf5b81f85e4e6df28dbdfa1
Lucas <lucasmattioli013@gmail.com>
2024-08-16 15:54:30 -0300

    Update Iphone.java

--- projeto /src /iphone /Iphone.java
+++ projeto /src /iphone /Iphone.java
@@ -1,1 +1,64 @@
+package iphone;
 
+import funcionalidades.telefone.AppTelefone;
+import funcionalidades.navegador.Google;
+import funcionalidades.navegador.Safari;
+import funcionalidades.musica.AppleMusic;
+
+public class Iphone extends AppTelefone {
+    private final AppleMusic appleMusic = new AppleMusic();
+    private final Google google = new Google();
+    private final Safari safari = new Safari();
+
+    public void tocarMusica() {
+        appleMusic.tocar();
+    }
+
+    public void pausarMusica() {
+        appleMusic.pausar();
+    }
+
+    public void selecionarMusica() {
+        appleMusic.selecionarMusica();
+    }
+
+    public void aumentarVolume() {
+        appleMusic.aumentarVolume();
+    }
+
+    public void abaixarVolume() {
+        appleMusic.abaixarVolume();
+    }
+
+    public void pesquisarNoGoogle() {
+        google.pesquisar();
+    }
+
+    public void exibirPaginaNoGoogle() {
+        google.exibirPagina();
+    }
+
+    public void adicionarNovaAbaNoGoogle() {
+        google.adicionarNovaAba();
+    }
+
+    public void atualizarPaginaNoGoogle() {
+        google.atualizarPagina();
+    }
+
+    public void pesquisarNoSafari() {
+        safari.pesquisar();
+    }
+
+    public void exibirPaginaNoSafari() {
+        safari.exibirPagina();
+    }
+
+    public void adicionarNovaAbaNoSafari() {
+        safari.adicionarNovaAba();
+    }
+
+    public void atualizarPaginaNoSafari() {
+        safari.atualizarPagina();
+    }
+}
