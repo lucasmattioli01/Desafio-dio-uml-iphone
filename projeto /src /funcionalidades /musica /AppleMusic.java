b55cb4918097d755452f8fbe7332c9a01eeb257c
Lucas <lucasmattioli013@gmail.com>
2024-08-16 15:51:41 -0300

    Update AppleMusic.java

--- projeto /src /funcionalidades /musica /AppleMusic.java
+++ projeto /src /funcionalidades /musica /AppleMusic.java
@@ -1,1 +1,24 @@
+package funcionalidades.musica;
+
+public class AppleMusic implements ReprodutorMusical{
+    public void tocar() {
+        System.out.println("Tocando música...");
+    }
+
+    public void pausar() {
+        System.out.println("Música pausada...");
+    }
+
+    public void selecionarMusica() {
+        System.out.println("Escolhendo música...");
+    }
+
+    public void aumentarVolume() {
+        System.out.println("Aumentando volume...");
+    }
+
+    public void abaixarVolume() {
+        System.out.println("Abaixando volume...");
+    }
+}
 
