1a32e7374c394d74af64bee3b6e87fdfef1f0513
Lucas <lucasmattioli013@gmail.com>
2024-08-16 15:53:33 -0300

    Update Safari.java

--- projeto /src /funcionalidades /navegador /Safari.java
+++ projeto /src /funcionalidades /navegador /Safari.java
@@ -1,1 +1,22 @@
+package funcionalidades.navegador;
 
+public class Safari implements Navegador {
+    public void pesquisar() {
+        System.out.println("Pesquisando no Safari...");
+    }
+
+    public void exibirPagina() {
+        System.out.println("Exibindo página no Safari...");
+
+    }
+
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba no Safari...");
+
+    }
+
+    public void atualizarPagina() {
+        System.out.println("Atualizando página no Safari...");
+
+    }
+}
